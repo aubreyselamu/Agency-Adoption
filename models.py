e499ced9a67af218bcc02e2bfceb2f66f108132c
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-12 13:26:06 -0500

    edit Pet model

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
+GENERIC_IMAGE = "https://mylostpetalert.com/wp-content/themes/mlpa-child/images/nophoto.gif"
+
 db = SQLAlchemy()
 
 def connect_db(app):
@@ -8,3 +10,16 @@ def connect_db(app):
 
 class Pet(db.Model):
     '''Pet'''
+
+    id = db.Column(db.Integer, autoincrement = True, primary_key=True)
+    name = db.Column(db.Text, nullable = False)
+    species = db.Column(db.Text, nullable = False)
+    photo_url = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable = False, default = True)
+
+    def image_url(self):
+        '''Return image'''
+
+        return self.photo_url or GENERIC_IMAGE
