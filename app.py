59b72c936efc6dba6f562ea147cb98544505ddc9
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-12 13:25:26 -0500

    created route for homepage

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "Yohanna_12"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///adopt"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 debug = DebugToolbarExtension(app)
+connect_db(app)
+
+@app.route('/')
+def homepage():
+    '''List all pets in homepage'''
+    
+    pets = Pet.query.all()
+    return render_template('pet_list.html', pets=pets)
