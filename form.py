0175a028ca5a9f8d571211a5b87bbb1b89bbb036
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-12 14:44:27 -0500

    created EditPetForm

--- form.py
+++ form.py
@@ -1,11 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, TextAreaField
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import Length, NumberRange, URL, Optional, InputRequired
 
 class AddPetForm(FlaskForm):
     '''Form for adding pets'''
 
-    pet_name = StringField("Pet Name")
-    species = SelectField("Species")
-    photo_url = StringField("Photo URL")
-    age = IntegerField("Age")
-    notes = TextAreaField("Notes")
+    pet_name = StringField("Pet Name", validators=[InputRequired()])
+    species = SelectField("Species", choices=[('cat', 'Cat'),('dog', 'Dog'),('porcupine', 'Porcupine')])
+    photo_url = StringField("Photo URL", validators = [URL(), Optional()])
+    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0,max=30)])
+    notes = TextAreaField("Notes", validators=[Optional(), Length(min=10)])
+
+class EditPetForm(FlaskForm):
+    photo_url = StringField("Photo URL", validators = [URL(), Optional()])
+    notes = TextAreaField("Notes", validators=[Optional(), Length(min=10)])
+    available = BooleanField("Available?")
